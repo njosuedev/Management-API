1d5fd80ec07021fb0478591d29d2c2660b2afe72
njosuedev <njosueofficial@gmail.com>
2025-02-27 06:00:39 +0200

    terminate

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -30,7 +30,7 @@ exports.find = (req, res) => {
     pool.getConnection((err, connection) => {
         if (err) throw err;
         let searchTerm = req.body.search;
-        connection.query('SELECT * FROM users WHERE  first_name LIKE ? OR last_name LIKE ?' , ['%'+ searchTerm +'%','%'+ searchTerm +'%'], (err, rows) => {
+        connection.query('SELECT * FROM users WHERE  first_name LIKE ? OR last_name LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
             connection.release();
             if (!err) {
                 res.render('home', { rows });
@@ -42,19 +42,19 @@ exports.find = (req, res) => {
     })
 }
 
-exports.form = (req,res) => {
+exports.form = (req, res) => {
     res.render('add-user')
 }
 
-exports.create = (req,res) => {
+exports.create = (req, res) => {
 
-    const {first_name,last_name,email,phone,comment} = req.body;
+    const { first_name, last_name, email, phone, comment } = req.body;
     pool.getConnection((err, connection) => {
         if (err) throw err;
-        connection.query('INSERT INTO users SET first_name = ? , last_name = ?,email = ?,phone = ?,comment = ?' ,[first_name,last_name,email,phone,comment], (err, rows) => {
+        connection.query('INSERT INTO users SET first_name = ? , last_name = ?,email = ?,phone = ?,comment = ?', [first_name, last_name, email, phone, comment], (err, rows) => {
             connection.release();
             if (!err) {
-                res.render('add-user',{alert: 'user added successfully.'});
+                res.render('add-user', { alert: 'user added successfully.' });
             }
             else {
                 console.log(err)
@@ -62,3 +62,62 @@ exports.create = (req,res) => {
         })
     })
 }
+
+exports.edit = (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query('SELECT * FROM users WHERE id = ?', [req.params.id], (err, rows) => {
+            connection.release();
+            if (!err) {
+                res.render('edit-user', { rows });
+            }
+            else {
+                console.log(err)
+            }
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    const { first_name, last_name, email, phone, comment } = req.body;
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query('UPDATE users SET first_name = ?, last_name = ?,email = ?,phone = ?,comment = ? WHERE id = ?', [first_name, last_name, email, phone, comment, req.params.id], (err, rows) => {
+            connection.release();
+            if (!err) {
+                pool.getConnection((err, connection) => {
+                    if (err) throw err;
+                    connection.query('SELECT * FROM users WHERE id = ?', [req.params.id], (err, rows) => {
+                        connection.release();
+                        if (!err) {
+                            res.render('edit-user', {rows, alert: `${first_name} has been updated.` });
+                        }
+                        else {
+                            console.log(err)
+                        }
+                    })
+                })
+            }
+            else {
+                console.log(err)
+            }
+        })
+    })
+}
+
+// delete
+
+// exports.delete = (req, res) => {
+//     pool.getConnection((err, connection) => {
+//         if (err) throw err;
+//         connection.query('DELETE FROM users WHERE id = ?', [req.params.id], (err, rows) => {
+//             connection.release();
+//             if (!err) {
+//                res.redirect('/')
+//             }
+//             else {
+//                 console.log(err)
+//             }
+//         })
+//     })
+// }
